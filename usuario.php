1a4fd5b3850e405784f0a1892d37792e2f8563ff
guifreri7 <guicfreri@gmail.com>
2026-03-09 22:29:41 -0300

    Add files via upload

---
+++ usuario.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="usuario1.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header-content">
+        <h2> Gestão de Usuários</h2>
+        <p>Visualize e gerecie as permissões dos usuários do sistema.</p>
+    </div>
+
+    <div class="table-responsive">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>E-Mail</th>
+                    <th>Acesso</th>
+                    <th>Status</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>01</td>
+                    <td>Guilherme Freri</td>
+                    <td>guicfreri@gmail.com</td>
+                    <td>Administrador</td>
+                    <td>Ativo</td>
+                    <td>Editar</td>
+                </tr>
+                <tr>
+                    <td>02</td>
+                    <td>Gustavo Puga</td>
+                    <td>puga@gmail.com</td>
+                    <td>Usuário</td>
+                    <td>Inativo</td>
+                    <td>Editar</td>
+                </tr>
+                <tr>
+                    <td>03</td>
+                    <td>Kayky</td>
+                    <td>CG125@gmail.com</td>
+                    <td>Moderador</td>
+                    <td>Inativo</td>
+                    <td>Editar</td>
+                </tr>
+                <tr>
+                    <td>02</td>
+                    <td>Joao Amaral</td>
+                    <td>amaralzinho@gmail.com</td>
+                    <td>Edior</td>
+                    <td>Ativo</td>
+                    <td>Editar</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
