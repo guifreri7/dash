f51655dd87a3e0cd9dcdcdcd0404cdc29d8839ec
guifreri7 <guicfreri@gmail.com>
2026-03-13 20:49:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Dashboard</title>
+<link rel="stylesheet" href="padrao.css">
+
+</head>
+
+<body>
+
+<div class="dashboard-container">
+
+    <!-- MENU -->
+    <div class="sidebar">
+        <h2>Painel</h2>
+
+        <ul>
+            <li>Início</li>
+            <li><a href="usuario.php">Usuários</a></li>
+            <li>Produtos</li>
+            <li>Relatórios</li>
+            <li>Configurações</li>
+        </ul>
+
+    </div>
+
+    <!-- CONTEÚDO -->
+    <div class="main">
+
+        <h1>Dashboard</h1>
+
+        <div class="cards">
+
+            <div class="card">
+                <h3>Usuários</h3>
+                <p>768</p>
+            </div>
+
+            <div class="card">
+                <h3>Vendas</h3>
+                <p>546</p>
+            </div>
+
+            <div class="card">
+                <h3>Pedidos</h3>
+                <p>124</p>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+</html>
