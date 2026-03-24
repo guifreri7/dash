af81ddd749f279b63f35b71bdbe4ac7d44e78252
guifreri7 <guicfreri@gmail.com>
2026-03-23 22:43:37 -0300

    Add files via upload

---
+++ categoria.php
@@ -0,0 +1,37 @@
+<div class="header-content">
+    <h2>Categorias</h2>
+    <p>Gerencie as categorias do sistema.</p>
+</div>
+
+<div class="table-responsive">
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Status</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>01</td>
+                <td>Tecnologia</td>
+                <td>Ativo</td>
+                <td>Editar</td>
+            </tr>
+            <tr>
+                <td>02</td>
+                <td>Esportes</td>
+                <td>Ativo</td>
+                <td>Editar</td>
+            </tr>
+            <tr>
+                <td>03</td>
+                <td>Notícias</td>
+                <td>Inativo</td>
+                <td>Editar</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
---
+++ postagem.php
@@ -0,0 +1,46 @@
+<div class="header-content">
+    <h2>Postagens</h2>
+    <p>Gerencie as postagens do sistema.</p>
+    <link rel="stylesheet" href="postagens.css">
+</div>
+
+<div class="table-responsive">
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Conteúdo</th>
+                <th>Categoria</th>
+                <th>Status</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>01</td>
+                <td>Unifev</td>
+                <td>Novas noticias sobre os cursos!</td>
+                <td>Tecnologia</td>
+                <td>Ativo</td>
+                <td>Editar</td>
+            </tr>
+            <tr>
+                <td>02</td>
+                <td>Final do campeonato</td>
+                <td>Time campeão da UNIFEV!</td>
+                <td>Esportes</td>
+                <td>Ativo</td>
+                <td>Editar</td>
+            </tr>
+            <tr>
+                <td>03</td>
+                <td>Política atual</td>
+                <td>Novas decisões na atual gestao da faculdade</td>
+                <td>Notícias</td>
+                <td>Inativo</td>
+                <td>Editar</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
